ef19953e2e558f4c29382106147effeb572d117a
ijaime096r <ijaime096r@fpdrioja.com>
2026-03-11 17:46:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 let preguntas = []
+let test = []
+
 let actual = 0
+let aciertos = 0
 
 async function cargar() {
 
     const respuesta = await fetch("preguntas.json")
     preguntas = await respuesta.json()
 
+    generarTest()
+
     mostrar()
+
+}
+
+function generarTest() {
+
+    // copiamos el array
+    test = [...preguntas]
+
+    // mezclamos preguntas
+    test.sort(() => Math.random() - 0.5)
+
+    // elegimos 20 preguntas
+    test = test.slice(0, 20)
 
 }
 
 function mostrar() {
 
-    const p = preguntas[actual]
+    const p = test[actual]
 
-    document.getElementById("pregunta").innerText = p.pregunta
+    document.getElementById("pregunta").innerText =
+        "Pregunta " + (actual + 1) + ":\n\n" + p.pregunta
 
     const contenedor = document.getElementById("opciones")
     contenedor.innerHTML = ""
@@ -22,11 +41,14 @@ function mostrar() {
     for (let i = 0; i < p.opciones.length; i++) {
 
         const boton = document.createElement("button")
+
         boton.innerText = p.opciones[i]
 
         boton.onclick = function() {
 
             if (i === p.correcta) {
+
+                aciertos++
 
                 alert("Correcta")
 
@@ -35,6 +57,8 @@ function mostrar() {
                 alert("Incorrecta")
 
             }
+
+            siguiente()
 
         }
 
@@ -48,9 +72,14 @@ function siguiente() {
 
     actual++
 
-    if (actual >= preguntas.length) {
+    if (actual >= test.length) {
+
+        alert("Test terminado. Aciertos: " + aciertos + " de " + test.length)
 
         actual = 0
+        aciertos = 0
+
+        generarTest()
 
     }
 
