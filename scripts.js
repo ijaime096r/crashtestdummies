34495a2ba69a16dd3fa192a276e6df89597b3d62
ijaime096r <ijaime096r@fpdrioja.com>
2026-03-14 14:35:51 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// control de autenticación
+
 import { auth } from "./firebase.js"
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
@@ -12,3 +14,113 @@ window.location.href = "login.html"
 }
 
 })
+
+
+// variables del test
+
+let preguntas = []
+let indice = 0
+let aciertos = 0
+
+
+// cargar preguntas
+
+fetch("preguntas.json")
+
+.then(res => res.json())
+
+.then(data => {
+
+preguntas = data
+
+mostrarPregunta()
+
+})
+
+
+// mostrar pregunta
+
+function mostrarPregunta() {
+
+let p = preguntas[indice]
+
+document.getElementById("pregunta").textContent = p.pregunta
+
+
+let contenedor = document.getElementById("respuestas")
+
+contenedor.innerHTML = ""
+
+
+for (let i = 0; i < p.opciones.length; i++) {
+
+let boton = document.createElement("button")
+
+boton.textContent = p.opciones[i]
+
+boton.onclick = function () {
+
+comprobar(i)
+
+}
+
+contenedor.appendChild(boton)
+
+}
+
+actualizarInfo()
+
+}
+
+
+// comprobar respuesta
+
+function comprobar(opcion) {
+
+let correcta = preguntas[indice].correcta
+
+if (opcion === correcta) {
+
+aciertos++
+
+alert("Correcto")
+
+}
+
+else {
+
+alert("Incorrecto")
+
+}
+
+}
+
+
+// siguiente pregunta
+
+document.getElementById("siguiente").onclick = function () {
+
+indice++
+
+if (indice >= preguntas.length) {
+
+alert("Test terminado")
+
+return
+
+}
+
+mostrarPregunta()
+
+}
+
+
+// actualizar contador
+
+function actualizarInfo() {
+
+let texto = "Pregunta " + (indice + 1) + " / " + preguntas.length + " | Aciertos: " + aciertos
+
+document.getElementById("info").textContent = texto
+
+}
