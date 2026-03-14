56ec8a0507da3cf12b8516d1ba9853251a353c05
ijaime096r <ijaime096r@fpdrioja.com>
2026-03-14 14:34:07 +0100

    Add Firebase configuration and authentication setup

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// configuración firebase
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyCHlLXfqIKXgO-SqABA-NSuiiBFQ6r0jnU",
+authDomain: "test-ferroviario-157ee.firebaseapp.com",
+projectId: "test-ferroviario-157ee",
+storageBucket: "test-ferroviario-157ee.firebasestorage.app",
+messagingSenderId: "93046466366",
+appId: "1:93046466366:web:4aefd781fe37968c4fae6f"
+
+}
+
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
