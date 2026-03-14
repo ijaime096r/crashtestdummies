be0b86e29088e53417c49a8c83ac37833c600a29
ijaime096r <ijaime096r@fpdrioja.com>
2026-03-14 14:35:05 +0100

    Refactor Firebase auth handling in scripts.js
    Removed Firebase initialization and replaced auth import.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { auth } from "./firebase.js"
 
-import {
-getAuth,
-onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
-
-
-const firebaseConfig = {
-
-apiKey: "TU_API_KEY",
-authDomain: "TU_PROYECTO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-appId: "TU_APP_ID"
-
-}
-
-
-const app = initializeApp(firebaseConfig)
-
-const auth = getAuth(app)
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
 
 
 onAuthStateChanged(auth, (user) => {
