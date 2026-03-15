189e5173a9cb686486fc1d1c3313394a3f7990e0
ijaime096r <ijaime096r@fpdrioja.com>
2026-03-15 11:30:06 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHIlXfqIKXq0-SqABA-NSuiiBFQ6rOjnU",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
+export const db = getFirestore(app)
